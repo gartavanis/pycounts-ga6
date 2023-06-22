f58c4d5fd4a889a8f2fec7247246257096916dbe
George Artavanis <ga6@verde.princeton.edu>
2023-06-22 10:50:17 -0400

    fix: correct module name

--- src/pycounts_ga6/__init__.py
+++ src/pycounts_ga6/__init__.py
@@ -3,5 +3,5 @@ from importlib.metadata import version
 __version__ = version("pycounts_ga6")
 
 # populate package namespace
-from pycounts.pycounts import count_words
-from pycounts.plotting import plot_words
+from pycounts_ga6.pycounts_ga6 import count_words
+from pycounts_ga6.plotting import plot_words
